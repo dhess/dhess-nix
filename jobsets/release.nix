eb96441bc9beb30e83b876fa0d61a4ccb217a02c
Drew Hess <src@drewhess.com>
2017-12-31 13:33:51 -0800

    Bump nixpkgs to latest nixos-unstable-small channel.
    This obsoletes the following packages from our ovrerlay:
    
    - certbot
    - simp_le
    
    It also removes the temporary 'release-lib.nix' hack that we needed
    for armv7l-linux support.

--- jobsets/release.nix
+++ jobsets/release.nix
@@ -1,23 +1,6 @@
-# The packageSet rigamarole is necessary for now because, until very
-# recently, release-lib.nix from Nixpkgs did not support armv7l-linux,
-# which we need. Until we're running that version of Nixpkgs (or
-# later), we have grabbed release-lib.nix from that revision and use
-# it locally; but since it imports both the packageSet and lib, we
-# have modified it slightly to be pure.
-#
-# See
-# https://github.com/NixOS/nixpkgs/commit/b1a2e1caef0879ff427ce34abbbbdea9dfb75d00
-
 let
 
-  # XXX dhess - When we've updated to the latest Nixpkgs, uncomment
-  # this line and remove the other bindings below.
-
-  #fixedNixPkgs = (import ../lib.nix).fetchNixPkgs;
-
-  lib = import ../lib.nix;
-  fixedNixPkgs = lib.fetchNixPkgs;
-  packageSet = (import fixedNixPkgs);
+  fixedNixPkgs = (import ../lib.nix).fetchNixPkgs;
 
 in
 
@@ -29,16 +12,8 @@ in
   }
 }:
 
-# XXX dhess - When we've updated to the latest Nixpkgs, uncomment this
-# import expression and remove the import expression below it (along
-# with our local, modified release-lib.nix).
-
-# with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
-#   inherit supportedSystems scrubJobs nixpkgsArgs;
-# };
-
-with import ./release-lib.nix {
-  inherit lib supportedSystems scrubJobs nixpkgsArgs packageSet;
+with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
+  inherit supportedSystems scrubJobs nixpkgsArgs;
 };
 
 let
@@ -50,14 +25,12 @@ let
       meta.description = "nixpkgs-quixoftic overlay packages (x86_64-linux)";
       meta.maintainer = lib.maintainers.dhess;
       constituents = with jobs; [
-        certbot.x86_64-linux
         ffmpeg-snapshot.x86_64-linux
         gitaly.x86_64-linux
         hyperscan.x86_64-linux
         libnet_1_1.x86_64-linux
         libprelude.x86_64-linux
         netsniff-ng.x86_64-linux
-        simp_le.x86_64-linux
         suricata.x86_64-linux
         trimpcap.x86_64-linux
         tsoff.x86_64-linux
@@ -71,10 +44,8 @@ let
       meta.maintainer = lib.maintainers.dhess;
       constituents = with jobs; [
         bb-org-overlays.armv7l-linux
-        certbot.armv7l-linux
         jemalloc.armv7l-linux
         linux_beagleboard.armv7l-linux
-        simp_le.armv7l-linux
       ];
     };
 
--- overlays/versions.nix
+++ overlays/versions.nix
@@ -7,12 +7,6 @@ let
 in rec
 {
 
-  # Cherry-picked from later nixpkgs.
-  certbot = callPackage ../pkgs/networking/certbot {};
-
   netsniff-ng = callPackage ../pkgs/networking/netsniff-ng {};
-
-  # Cherry-picked from later nixpkgs.
-  simp_le = callPackage ../pkgs/networking/simp_le {};
 
 }
