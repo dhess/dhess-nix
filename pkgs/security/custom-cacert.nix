bbb56a9d32f660e74de857abceb4450985336276
Drew Hess <dhess-src@quixoftic.com>
2018-04-19 15:04:03 -0700

    Fix the mkCacert lib function for newer Nixpkgs.

--- pkgs/security/custom-cacert.nix
+++ pkgs/security/custom-cacert.nix
@@ -23,6 +23,10 @@ cacert.overrideAttrs (oldAttrs: {
   installPhase = ''
     mkdir -pv $out/etc/ssl/certs
     cat ca-bundle.crt ${extraCAs} > $out/etc/ssl/certs/ca-bundle.crt
+    # install individual certs in unbundled output
+    mkdir -pv $unbundled/etc/ssl/certs
+    cp -v *.crt $unbundled/etc/ssl/certs
+    rm -f $unbundled/etc/ssl/certs/ca-bundle.crt  # not wanted in unbundled
   '';
 
 })
