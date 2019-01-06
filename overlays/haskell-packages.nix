7acbe4cff7fae6a8c5ec25e5482fac89ee0bf3ea
Drew Hess <src@drewhess.com>
2019-01-06 13:08:00 -1000

    dontCheck some ghc844 packages.

--- overlays/haskell-packages.nix
+++ overlays/haskell-packages.nix
@@ -4,11 +4,18 @@ let
 
   inherit (self.haskell.lib) doJailbreak dontCheck;
 
-  hpDhallPackages = self.haskell.lib.properExtend super.haskellPackages (self: super:
+  dhall-to-cabal-packages = self.haskell.lib.properExtend super.haskellPackages (self: super:
     {
       # dhall runs network tests.
       dhall = dontCheck (doJailbreak (super.callPackage ../pkgs/haskell/dhall/1.18.0.nix {}));
       dhall-to-cabal = doJailbreak (super.callPackage ../pkgs/haskell/dhall-to-cabal {});
+    }
+  );
+
+  dhall-nix-packages = self.haskell.lib.properExtend super.haskell.packages.ghc844 (self: super:
+    {
+      cereal = dontCheck super.cereal;
+      Diff = dontCheck super.Diff;
     }
   );
 
@@ -23,6 +30,6 @@ in
   # causes a nasty (internal?) GHC bug.
   pinpon = self.haskell.lib.justStaticExecutables (self.haskell.lib.dontCheck self.haskellPackages.pinpon);
 
-  dhall-to-cabal = self.haskell.lib.justStaticExecutables hpDhallPackages.dhall-to-cabal;
-  dhall-nix = self.haskell.lib.justStaticExecutables super.haskell.packages.ghc844.dhall-nix;
+  dhall-to-cabal = self.haskell.lib.justStaticExecutables dhall-to-cabal-packages.dhall-to-cabal;
+  dhall-nix = self.haskell.lib.justStaticExecutables dhall-nix-packages.dhall-nix;
 }
