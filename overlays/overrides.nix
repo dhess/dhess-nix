ce6fcb1d5466372aa8d4cb97ddf51d2fb587c2e0
Drew Hess <src@drewhess.com>
2019-01-08 10:51:49 -1000

    Remove the wpa_supplicant patch.
    It keeps creating this issue in downstream repos that use this
    overlay:
    
    https://github.com/NixOS/nixpkgs/issues/34086

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -14,21 +14,11 @@ in
   };
 
 
-  # Enable TLS v1.2 and the eapol_test binary in wpa_supplicant.
+  # Enable TLS v1.2 in wpa_supplicant.
 
   wpa_supplicant = super.wpa_supplicant.overrideAttrs (drv: {
-
     extraConfig = drv.extraConfig + ''
      CONFIG_TLSV12=y
     '';
-
-    # XXX dhess - lib.unique is needed here to work around this issue:
-    # https://github.com/NixOS/nixpkgs/issues/34086
-    # Otherwise, our patch may be applied twice in some circumstances.
-
-    patches = super.lib.unique (drv.patches ++ [
-      ./patches/wpa_supplicant/Makefile.eapol_test.patch
-    ]);
-
   });
 }
