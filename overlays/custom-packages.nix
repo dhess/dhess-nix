2411c29edee6d361db86e3801964b9a42d85670d
Drew Hess <src@drewhess.com>
2019-01-08 18:48:26 -1000

    Refactor a bit.

--- overlays/custom-packages.nix
+++ overlays/custom-packages.nix
@@ -4,8 +4,6 @@ let
 
   inherit (super) callPackage;
 
-in
-{
   debian-ppp = callPackage ../pkgs/networking/debian-ppp {};
 
   libprelude = callPackage ../pkgs/development/libraries/libprelude {};
@@ -20,4 +18,13 @@ in
   trimpcap = callPackage ../pkgs/misc/trimpcap {};
 
   tsoff = callPackage ../pkgs/networking/tsoff {};
+
+in
+{
+  inherit debian-ppp;
+  inherit libprelude;
+  inherit unbound-block-hosts;
+  inherit suricata;
+  inherit trimpcap;
+  inherit tsoff;
 }
