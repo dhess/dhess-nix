6c44605f31ebc6600ef5af2b25880664575ec5a2
Drew Hess <src@drewhess.com>
2019-02-03 20:55:55 +0000

    Bump dependencies.

--- jobsets/release.nix
+++ jobsets/release.nix
@@ -20,25 +20,6 @@ with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
 
 let
 
-  ## Aggregates are handy for defining jobs (especially for subsets of
-  ## platforms), but they don't provide very useful information in
-  ## Hydra, especially when they die. We use aggregates here to define
-  ## set of jobs, and then splat them into the output attrset so that
-  ## they're more visible in Hydra.
-
-  enumerateConstituents = aggregate: lib.listToAttrs (
-    map (d:
-           let
-             name = (builtins.parseDrvName d.name).name;
-             system = d.system;
-           in
-             { name = name + "." + system;
-               value = d;
-             }
-         )
-        aggregate.constituents
-  );
-
   jobs = {
 
     x86_64-linux = pkgs.releaseTools.aggregate {
@@ -150,6 +131,3 @@ in
   inherit (jobs) x86_64-darwin;
   inherit (jobs) aarch64-linux;
 }
-// enumerateConstituents jobs.x86_64-linux
-// enumerateConstituents jobs.x86_64-darwin
-// enumerateConstituents jobs.aarch64-linux
