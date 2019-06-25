995b31ab45b052dbe4605eb24defa70e7af8e8dd
Drew Hess <src@drewhess.com>
2019-06-25 17:11:47 +0200

    Fix Hydra patch sha256's; re-add the Hydra test fix patch.

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -23,20 +23,24 @@ in
   hydra = super.hydra.overrideAttrs (drv: {
     patches = [
       (super.fetchpatch {
+        url = "https://github.com/NixOS/hydra/commit/4171ab4c4fd576c516dc03ba64d1c7945f769af0.patch";
+        sha256 = "1fxa2459kdws6qc419dv4084c1ssmys7kqg4ic7n643kybamsgrx";
+      })
+      (super.fetchpatch {
         url = "https://github.com/input-output-hk/hydra/commit/ed87d2ba82807d30d91d70a88cda276083ef4e79.patch";
-        sha256 = "0mn37kv3xwjgafmrs3hms1r8282zqyzfydskkia64qkrf3slh3wj";
+        sha256 = "0mzmm480cs085wbbn1ha6ml164v0dslfh0viak73mc84rvl00ckb";
       })
       (super.fetchpatch {
         url = "https://github.com/input-output-hk/hydra/commit/96ec35acface848c546b67e6b835094412a488d9.patch";
-        sha256 = "0hjp9f8ldfkvia8mcwg12mxq6bpygqbkqzs3l0zv1gyhxanjfvrh";
+        sha256 = "0ax4bn1ivk5yg5mbdkcqbqc2vrjlyk9km2rzg1k1kqc81ka570a2";
       })
       (super.fetchpatch {
         url = "https://github.com/input-output-hk/hydra/commit/11db34b6a9243a428b3d5935c65ac13a8080d02c.patch";
-        sha256 = "0dyvbx2gynzd8jl3ip3aawirb9kqf824k7g0jgh5qircgl44hd5m";
+        sha256 = "10b9ywif14gncf1j9647hhpbqh67yy0pv1c8f38f08v68a83993s";
       })
       (super.fetchpatch {
         url = "https://github.com/input-output-hk/hydra/commit/0768891e3cd3ef067d28742098f1dea8462fca75.patch";
-        sha256 = "0i9mq1f9n0q75g0caxm8b10bmm80mrfywwq4wznv855fkprrcjry";
+        sha256 = "0m4009pmi4sl0vwq6q98bzp4hpnfr4ww1j27czwcazbda7l8fdzy";
       })
     ];
   });
