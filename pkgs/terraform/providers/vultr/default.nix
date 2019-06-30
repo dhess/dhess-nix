284c0cedbb9e48f84e31bd4527628adf45621bfe
Drew Hess <src@drewhess.com>
2019-06-30 17:08:08 +0100

    Bump terraform-provider-vultr.

--- pkgs/terraform/providers/vultr/default.nix
+++ pkgs/terraform/providers/vultr/default.nix
@@ -13,8 +13,8 @@ buildGoPackage rec {
   src = fetchFromGitHub {
     owner = "squat";
     repo = "terraform-provider-vultr";
-    rev = "v${version}";
-    sha256 = "1cfylyfxrns81jx1av60yrdc273ngxhwm2xzfqlvlxxw05p6lcrk";
+    rev = "48cd5bca883dd8aa0034f9cf054c642b8005ed72";
+    sha256 = "05qrvzqpnk9h4y69dykk1y9nbi7rhw9fy2dan2p5gpxmrzx55znj";
   };
 
   # Terraform allow checking the provider versions, but this breaks
