5874de079e06c5c2a01baba48c3ed8c5809a5ae1
Drew Hess <src@drewhess.com>
2019-09-16 15:37:52 +0100

    Fix upstream ntpd module hash.

--- modules/services/ntpd/default.nix
+++ modules/services/ntpd/default.nix
@@ -110,7 +110,7 @@ in
   config = mkIf config.services.ntp.enable {
 
     dhess-nix.assertions.moduleHashes."services/networking/ntp/ntpd.nix" =
-      "800a4a18c31007f9d99fd07176f4b2b46f790a1bec74471bb1500a685c2125c6";
+      "030f4eddd4f478b9982eac3871965e33d2af58880c8d19a9fd22aaf959a4f7d0";
 
     # Make tools such as ntpq available in the system path.
     environment.systemPackages = [ pkgs.ntp ];
