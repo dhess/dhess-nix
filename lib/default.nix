2c7efd3afd44cb87991c2354c13ec04a7783e4b8
Drew Hess <src@drewhess.com>
2019-11-03 16:17:35 +0000

    niv all the things.

--- lib/default.nix
+++ lib/default.nix
@@ -1,37 +1,29 @@
 let
 
-  # From https://github.com/input-output-hk/iohk-ops/blob/e6f1ae95cdbfdd5c213aa0b9a1ef67150febc503/lib.nix
-  
-  fixedDhessLibNix =
-  let
-    try = builtins.tryEval <dhess_lib_nix>;
-  in
-    if try.success
-      then builtins.trace "Using <dhess_lib_nix>" try.value
-      else (import ./fetch-github.nix) { jsonSpec = builtins.readFile ./dhess-lib-nix-src.json; };
+  sources = import ../nix/sources.nix;
 
-  dhess-lib-nix = (import fixedDhessLibNix) {};
+  dhess-lib-nix = (import sources.dhess-lib-nix) {};
   inherit (dhess-lib-nix) lib haskell;
   inherit (lib.fetchers) fixedNixpkgs;
   inherit (lib.dhess-lib-nix) nixpkgs;
 
-  fixedNixOps = lib.fetchers.fixedNixSrc "nixops_override" ./nixops-src.json;
+  fixedNixOps = sources.nixops;
 
-  fixedHpio = lib.fetchers.fixedNixSrc "hpio_override" ./hpio-src.json;
+  fixedHpio = sources.hpio;
   hpio = (import fixedHpio) {};
 
-  fixedPinPon = lib.fetchers.fixedNixSrc "pinpon_override" ./pinpon-src.json;
+  fixedPinPon = sources.pinpon;
   pinpon = (import fixedPinPon) {};
 
-  fixedMellon = lib.fetchers.fixedNixSrc "mellon_override" ./mellon-src.json;
+  fixedMellon = sources.mellon;
   mellon = (import fixedMellon) {};
 
-  fixedAllHies = lib.fetchers.fixedNixSrc "all_hies_override" ./all-hies-src.json;
+  fixedAllHies = sources.all-hies;
   all-hies = (import fixedAllHies) {};
 
-  fixedIHaskell = lib.fetchers.fixedNixSrc "ihaskell_override" ./ihaskell-src.json;
+  fixedIHaskell = sources.IHaskell;
 
-  fixedLorri = lib.fetchers.fixedNixSrc "lorri_override" ./lorri.json;
+  fixedLorri = sources.lorri;
 
   overlays = [
     dhess-lib-nix.overlays.all
