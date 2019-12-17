1dc8b507d4a92aeda8eda7d0ba36937b04634240
Drew Hess <src@drewhess.com>
2019-12-17 16:33:14 +0000

    Bump oauth2_proxy to new release.

--- pkgs/oauth2_proxy/default.nix
+++ pkgs/oauth2_proxy/default.nix
@@ -5,7 +5,7 @@
 
 buildGoModule rec {
   pname = "oauth2_proxy";
-  version = "4.0.0";
+  version = "4.1.0";
 
   goPackagePath = "github.com/pusher/${pname}";
   modSha256 = "0zjmah0v8gkz4mn5z46yjz8zng6y397vbz6xm36p6z2p90n6s14n";
@@ -13,8 +13,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     repo = pname;
     owner = "pusher";
-    sha256 = "1imygy7y5v96szqha2pbi9mbf3j5dbavibp2iwnhp9b36447b0nb";
-    rev = "9f920b0fc151ffdc057540c7dfdc87c612a2f8e7";
+    sha256 = "0pl747lf2bxfd75sf1iv9a7lpcv70nvzva4d3ch8k2gi0zlmvvca";
+    rev = "8165f6c4835d5c817ca6cd1853698feb136c77b3";
   };
 
   meta = with lib; {
