6ba53792a37f91e3d4b5881320dbc8165456f7ec
Drew Hess <src@drewhess.com>
2020-01-14 13:44:33 +0000

    Fix URL for bytesmith.

--- pkgs/haskell/bytesmith/0.3.3.0.nix
+++ pkgs/haskell/bytesmith/0.3.3.0.nix
@@ -6,7 +6,7 @@ mkDerivation {
   pname = "bytesmith";
   version = "0.3.3.0";
   src = fetchgit {
-    url = "git@github.com:andrewthad/bytesmith.git";
+    url = "https://github.com/andrewthad/bytesmith.git";
     sha256 = "1dbw9hpzriac4vf7jgnkfssm9m1y2sxs88rc04svxlsnfnhx90ym";
     rev = "a5800617498f6c861c2a95bbf5f03af8477b48df";
     fetchSubmodules = true;
