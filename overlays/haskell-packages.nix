72cb137387a12469741d3cff937c31c98e32339e
Drew Hess <src@drewhess.com>
2020-01-16 09:52:24 +0000

    Attempt to jailbreak ghcide.

--- overlays/haskell-packages.nix
+++ overlays/haskell-packages.nix
@@ -48,7 +48,7 @@ let
 
     generic-lens = dontCheck super.generic-lens_1_2_0_1;
 
-    ghcide = dontCheck (super.callPackage ../pkgs/haskell/ghcide {});
+    ghcide = dontCheck (doJailbreak (super.callPackage ../pkgs/haskell/ghcide {}));
 
     # Ironically, haddock-api doesn't haddock.
     haddock-api =  dontHaddock (doJailbreak super.haddock-api);
